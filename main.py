8d95718517aff0fd0e6316825b414c49f3908300
Nikhil Joshi <nikhiljoshi1012@gmail.com>
2025-01-13 18:34:58 +0530

    Add basic web browser functionality using PyQt5

---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://google.com"))  # replace with your website
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # Navigation Bar
+        nav_bar = QToolBar()
+        self.addToolBar(nav_bar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        nav_bar.addAction(back_btn)
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        nav_bar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        nav_bar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        nav_bar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        nav_bar.addWidget(self.url_bar)
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("http://google.com"))  # replace with your website
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("My App")
+window = MainWindow()
+app.exec_()
